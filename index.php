45bd02e2be5bfd3a5d6b695eabbfa06095535867
Emil Johannesson <emil.johannesson@utb.ecutbildning.se>
2021-10-31 14:43:47 +0100

    Det nödvändiga + lite templates

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Inlämningsuppgift 2 Emil Johannesson</title>
+    <link rel="stylesheet" href="/wp-content/themes/inl2EmilJ/style.css">
+</head>
+<body>
+<?php
+echo "Hello World";
+?>
+</body>
+</html>
